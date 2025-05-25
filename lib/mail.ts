43fbc3b5d31b22ba640c87b88bb4f03c2c2e9e94
P2897333 <raghavendra.grandhe@charter.com>
2025-05-25 12:58:30 -0400

    fix: remove nodemailer code and exports to resolve Next.js 'use server' error

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,16 +1,3 @@
 "use server"
 
-import nodemailer from "nodemailer"
-
-// Create a transporter object using SMTP
-const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number.parseInt(process.env.EMAIL_PORT || "587"),
-  secure: process.env.EMAIL_SECURE === "true",
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD,
-  },
-})
-
-export default transporter
+// Removed all code and exports to resolve the Next.js 'use server' error.
